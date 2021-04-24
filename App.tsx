5176dbfd78c2d881d0fba0d6440d954181d258cc
Rodrigo <rodrigolemosrl@hotmail.com>
2021-04-24 12:21:30 -0300

    feat: add navigation

--- App.tsx
+++ App.tsx
@@ -2,9 +2,7 @@ import React from 'react';
 import AppLoading from 'expo-app-loading';
 import { useFonts, Jost_400Regular, Jost_600SemiBold } from '@expo-google-fonts/jost';
 
-import { Welcome } from './src/pages/Welcome';
-import { UserIdentification } from './src/pages/UserIdentification';
-import { Confirmation } from './src/pages/Confirmation';
+import Routes from './src/routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -17,6 +15,6 @@ export default function App() {
   }
 
   return (
-    <Confirmation />
+    <Routes />
   );
 }
