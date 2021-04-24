825c06eb0bacb3b1b1c04f1c8d86553257bf704a
Rodrigo <rodrigolemosrl@hotmail.com>
2021-04-24 19:45:29 -0300

    refactor: confirmation as reusable page

--- src/pages/Confirmation.tsx
+++ src/pages/Confirmation.tsx
@@ -1,31 +1,48 @@
 import React from 'react';
 import { SafeAreaView, Text, View, StyleSheet } from 'react-native';
-import { useNavigation } from '@react-navigation/core';
+import { useNavigation, useRoute } from '@react-navigation/core';
 
 import colors from '../../styles/colors';
 import fonts from '../../styles/fonts';
 import { Button } from '../components/Button';
 
+interface Params {
+  title: string;
+  subtitle: string;
+  buttonTitle: string;
+  icon: 'smile' | 'hug';
+  nextScreen: string;
+}
+
+const emojis = {
+  smile: '😄',
+  hug: '🤗'
+}
+
 export function Confirmation() {
   const navigation = useNavigation();
+  const routes = useRoute();
+  const { title, subtitle, buttonTitle, icon, nextScreen } = routes.params as Params;
 
   function handleMoveOn() {
-    navigation.navigate('PlantSelect')    ;
+    navigation.navigate(nextScreen);
   }
 
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.content}>
-        <Text style={styles.emoji}>😄</Text>
+        <Text style={styles.emoji}>
+          {emojis[icon]}
+        </Text>
         <Text style={styles.title}>
-          Prontinho!
+          {title}
         </Text>
         <Text style={styles.subtitle}>
-          Agora vamos começar a cuidar das suas plantinhas com muito cuidado.
+          {subtitle}
         </Text>
         <View style={styles.footer}>
           <Button
-            title="Começar"
+            title={buttonTitle}
             onPress={handleMoveOn}
           />
         </View>
